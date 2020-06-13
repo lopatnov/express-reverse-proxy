aa68280bd7e4e9bf4271b648acbc4c4f3d282738
Oleksandr Lopatnov <oleksandr@lopatnov.cv.ua>
2020-06-13 04:52:17 +0300

    Modified Static Files Server

--- server.js
+++ server.js
@@ -1,16 +1,61 @@
+/* jslint es6:true */
+
 import path from 'path';
 import express from 'express';
-import pkg from './package.json';
+import config from './config.json';
+import morgan from 'morgan';
 
 const app = express();
 const host = 'localhost';
 const port = 8080;
 
-pkg.folders.forEach((folderName) => {
-    const folder = path.join(__dirname, folderName);
-    console.info(`Folder: ${folder}`);
-    app.use(express.static(folder));
-});
+function addStaticFolderByName(urlPath, folder) {
+    "use strict";
+    let folderPath = folder;
+    if (!path.isAbsolute(folder)){
+        folderPath = path.join(__dirname, folder);
+    }
+    if (urlPath) {
+        app.use(urlPath, express.static(folderPath));
+    } else {
+        app.use(express.static(folderPath));
+    }
+    console.log(`[static] ${urlPath || '/'} = ${folderPath}`);
+}
+
+function addMappedStaticFolders(rootPath, folders) {
+    "use strict";
+    rootPath = rootPath || '';
+    const keys = Object.getOwnPropertyNames(folders);
+    keys.forEach((key) => {
+        const folderPath = rootPath + key;
+        addStaticFolder(folderPath, folders[key]);
+    });
+}
+
+function addStaticFolders(path, folders) {
+    "use strict";    
+    folders.forEach(function (folder) {
+        addStaticFolder(path, folder);
+    });
+}
+
+function addStaticFolder(path, folder) {
+    "use strict";
+    if ('string' === typeof(folder)) {
+        addStaticFolderByName(path, folder);
+    } else if (Array.isArray(folder)) {
+        addStaticFolders(path, folder);
+    } else if (folder instanceof Object) {
+        addMappedStaticFolders(path, folder);
+    }
+}
+
+if (config && config.folders) {
+    addStaticFolder(null, config.folders);
+}
+
+app.use(morgan('combined'));
 
 app.listen(port, function () {
     console.log(`Server listening on ${host}:${port}`);
