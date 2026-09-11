a26d4a6e6bfb6b02ac88523f23498d132ccf86c9
Oleksandr Lopatnov <oleksandr@lopatnov.cv.ua>
2026-09-11 23:56:56 +0300

    improved demo cgi script

--- demo/cgi-bin/test-process.js
+++ demo/cgi-bin/test-process.js
@@ -1,8 +1,15 @@
 const method = process.env.REQUEST_METHOD || 'UNKNOWN';
 const query = process.env.QUERY_STRING || '';
 
-process.stdout.write('Content-Type: text/plain\r\n');
+process.stdout.write('Content-Type: text/html\r\n');
 process.stdout.write('X-Runtime: process\r\n');
 process.stdout.write('\r\n');
-process.stdout.write(`Executed inside child process. Method: ${method}, Query: ${query}\n`);
+process.stdout.write(
+  `<html>
+    <body>
+        <h1>CGI Script Output</h1>
+        <p>Executed inside child process. Method: ${method}, Query: ${query}</p>
+    </body>
+  </html>`,
+);
 process.stdin.pipe(process.stdout);
